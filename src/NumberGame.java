52e955974c6dcee5bba53f572be0a148070b7c30
Singh28priyanka <168950140+Singh28priyanka@users.noreply.github.com>
2025-10-03 00:41:28 +0530

    init commit

---
+++ src/NumberGame.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Random;
+public class NumberGame {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        Random ran =new Random();
+        int LowerBound=1;
+        int UpperBound=100;
+        int randomNumber=ran.nextInt(UpperBound-LowerBound+1)+ LowerBound;
+        int guess ,attempts=0,maxAttempts=5;
+        System.out.println("Guess the number between  " + LowerBound + " and " + UpperBound );
+        while (attempts < maxAttempts){
+            System.out.print("Enter your guess:");
+            guess=input.nextInt();
+            attempts++;
+
+            if(guess==randomNumber){
+                System.out.println("Correct! Right guess!!!!" + attempts +"attempts.");
+                break;
+            }
+            else if(guess > randomNumber){
+                System.out.println("Too High! Try again.");
+            }
+            else {
+                System.out.println("Too Low! Try again.");
+            }
+
+        }
+        if(attempts==maxAttempts){
+            System.out.println(" Out of attempts! the number was: " + randomNumber);
+        }
+        input.close();
+
+    }
+}
